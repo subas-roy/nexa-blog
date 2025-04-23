411ab17dbe43d6aacab5c7a05fc2a0a71f1ec81f
subas-roy <subasroy46@gmail.com>
2025-04-23 11:17:00 +0600

    Caching and revalidating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import LatestBlogs from '@/components/LatestBlogs/LatestBlogs';
 
 const HomePage = async () => {
-  const res = await fetch('http://localhost:5000/blogs');
+  const res = await fetch('http://localhost:5000/blogs', {
+    next: { revalidate: 30 }, // revalidate every 30 seconds meaning it will fetch the data every 30 seconds called ISR (Incremental Static Regeneration) also called SSG (Static Site Generation). Caching is done by Next.js.
+  });
   const blogs = await res.json();
   // console.log(blogs);
   return (
