cda33e6fad6ebc91c8da1a33d8d2f4fd250fa26a
subas-roy <subasroy46@gmail.com>
2025-04-23 12:06:54 +0600

    Show off all the blogs using SSR

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,29 @@
+import BlogCard from '@/components/ui/BlogCard';
+import { Blog } from '@/types';
+
+const BlogsPage = async () => {
+  const res = await fetch('http://localhost:5000/blogs', { cache: 'no-store' }); // SSR (Server Side Rendering) fetch data on every request. This is useful when you want to fetch data that changes frequently or when you want to ensure that the data is always up to date.
+  const blogs = await res.json();
+  // console.log(blogs);
+
+  return (
+    <div className="w-[90%] mx-auto">
+      <h1 className="text-3xl text-center my-5 font-bold">
+        Explore All <span className="text-teal-600">Blogs</span>
+      </h1>
+      <p className="text-center text-gray-400 w-2/5 mx-auto">
+        <i>
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+      <div className="grid grid-cols-3 gap-6 my-5">
+        {blogs.map((blog: Blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
