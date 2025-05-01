9a30eea8fd478de369949512bc83fb0ad8e81048
subas-roy <subasroy46@gmail.com>
2025-05-02 00:46:23 +0600

    Created RESTful APIs POST

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -28,3 +28,18 @@ const blogs = [
 export const GET = async () => {
   return NextResponse.json(blogs);
 };
+
+export const POST = async (request: Request) => {
+  const blog = await request.json();
+  const newBlog = {
+    ...blog,
+    id: blogs.length + 1,
+  };
+
+  blogs.push(newBlog);
+
+  return new NextResponse(JSON.stringify(newBlog), {
+    status: 201,
+    headers: { 'Content-Type': 'application/json' },
+  });
+};
